f72dd102a0bee9128d3ed2b3080e0e955197ccb8
hharrold <teffin@mail.ru>
2019-10-17 17:43:30 +0300

    add path_op - fix norm

--- source/path_operations.c
+++ source/path_operations.c
@@ -48,12 +48,22 @@ void		add_path(t_path **path, t_path *new)
 	*path = new;
 }
 
+static int	was_in_room_1(t_graph **graph, t_link *temp, t_trace *trace, int i)
+{
+	if (((trace->traces_1)[(trace->traces_0)[i]] != -1
+		 && temp->status + temp->reverse->status == 1
+		 && graph[0][(trace->traces_0)[i]].visited == 1)
+		|| trace->traces_0[trace->traces_0[i]] == -1)
+		return (1);
+	return (0);
+
+}
+
 void		restoration_path(t_graph **graph, t_info *info, t_trace *trace)
 {
 	int			i;
 	t_link		*temp;
 
-	//printf("%d, %d\n", info->ind_start, info->ind_end);
 	i = info->ind_end;
 	while (i != info->ind_start)
 	{
@@ -63,10 +73,9 @@ void		restoration_path(t_graph **graph, t_info *info, t_trace *trace)
 			if (temp->node == i)
 			{
 				temp->status = 0;
-				//printf("SSSSS %d", temp->status + temp->reverse->status);
-				if (((trace->traces_1)[(trace->traces_0)[i]] != -1 && temp->status + temp->reverse->status == 1 && graph[0][(trace->traces_0)[i]].visited == 1)
-				|| trace->traces_0[trace->traces_0[i]] == -1)
-					(trace->traces_0)[(trace->traces_0)[i]] = (trace->traces_1)[(trace->traces_0)[i]];
+				if(was_in_room_1(graph, temp, trace, i))
+					(trace->traces_0)[(trace->traces_0)[i]] =
+							(trace->traces_1)[(trace->traces_0)[i]];
 				if ((trace->traces_0)[i] != info->ind_start)
 					graph[0][(trace->traces_0)[i]].visited = 1;
 				break ;
@@ -74,9 +83,5 @@ void		restoration_path(t_graph **graph, t_info *info, t_trace *trace)
 			temp = temp->next;
 		}
 		i = (trace->traces_0)[i];
-	//	printf("[%d] ", i);
-//		if (i == 4512)
-//			printf("\n!!!!!!!!!!!!!!%d(%s) %d- tr_0 %d tr_1 %d\n", i, graph[0][i].name, graph[0][i].visited, (trace->traces_0)[i], (trace->traces_1)[i]);
 	}
-//	printf("\n!!!\n");
 }
